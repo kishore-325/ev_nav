ea9f4caebbe11445ed8372aa0294f09da4d9eafe
kishore-server <kishorerock325@gmail.com>
2026-02-26 15:31:09 +0900

    training complete for linear+inverse+0.99

--- perception/train.py
+++ perception/train.py
@@ -135,6 +135,7 @@ def run():
 
             optimizer.zero_grad()
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
             train_loss += loss.item() * event.size(0)
