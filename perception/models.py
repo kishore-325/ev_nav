bfb4fb7a43d13a00afbbcf8aeb463d660a74f303
kishore-server <kishorerock325@gmail.com>
2026-03-22 23:22:46 +0900

    train complete without predicting zeros

--- perception/models.py
+++ perception/models.py
@@ -55,7 +55,7 @@ class OrigUNet(nn.Module):
         self.d42 = nn.Conv2d(32, 32, kernel_size=3, padding=0)
 
         self.out_conv = nn.Conv2d(32, 1, kernel_size=1)     # (N, 1, 64, 148)
-        nn.init.zeros_(self.out_conv.weight)
+        nn.init.kaiming_normal_(self.out_conv.weight, nonlinearity='relu')
         nn.init.constant_(self.out_conv.bias, -2.20)  # sigmoid(-2.20) ≈ 0.10 = midpoint of [0, 0.20] target range
 
         self.nonlin = nn.ReLU()
