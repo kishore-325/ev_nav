e7a58a3bc7ab3ec31d5c2d450b826f059f680a1d
kishore-server <kishorerock325@gmail.com>
2026-03-24 20:14:15 +0900

    second training using cma-es tuned hyperparameter is complete

--- perception/evaluate.py
+++ perception/evaluate.py
@@ -9,7 +9,7 @@ sys.path.append(os.environ['PROJECT_PATH'])
 from perception.models import OrigUNet
 from perception.dataset import EventDepthDataset
 
-TEST_DATASETS_DIR = os.path.join(os.environ['PROJECT_PATH'], 'datasets', 'Test')
+TEST_DATASETS_DIR = os.path.join(os.environ['PROJECT_PATH'], 'datasets', 'Train')
 CKPT_PATH         = os.path.join('/home/srinivasan/ev_nav', 'perception', 'checkpoints', 'best.pth')
 DEPTH_THRESH      = 0.20
 BATCH_SIZE        = 64
